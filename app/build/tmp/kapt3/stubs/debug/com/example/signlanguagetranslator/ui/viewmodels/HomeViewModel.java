1942e8761c6a6efdeae0474fdb9db2cf5337383b
varun <varunkumar2004.vk@gmail.com>
2024-08-26 17:54:43 +0530

    first commit main

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/signlanguagetranslator/ui/viewmodels/HomeViewModel.java
@@ -0,0 +1,27 @@
+package com.example.signlanguagetranslator.ui.viewmodels;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000,\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\b\u0007\u0018\u00002\u00020\u0001B\u0007\b\u0007\u00a2\u0006\u0002\u0010\u0002J\u000e\u0010\n\u001a\u00020\u000b2\u0006\u0010\f\u001a\u00020\rJ\u0006\u0010\u000e\u001a\u00020\u000bR\u0014\u0010\u0003\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0017\u0010\u0006\u001a\b\u0012\u0004\u0012\u00020\u00050\u0007\u00a2\u0006\b\n\u0000\u001a\u0004\b\b\u0010\t\u00a8\u0006\u000f"}, d2 = {"Lcom/example/signlanguagetranslator/ui/viewmodels/HomeViewModel;", "Landroidx/lifecycle/ViewModel;", "()V", "_state", "Lkotlinx/coroutines/flow/MutableStateFlow;", "Lcom/example/signlanguagetranslator/ui/viewmodels/HomeState;", "state", "Lkotlinx/coroutines/flow/StateFlow;", "getState", "()Lkotlinx/coroutines/flow/StateFlow;", "onQueryChange", "", "msg", "", "sendPrompt", "app_debug"})
+@dagger.hilt.android.lifecycle.HiltViewModel
+public final class HomeViewModel extends androidx.lifecycle.ViewModel {
+    @org.jetbrains.annotations.NotNull
+    private final kotlinx.coroutines.flow.MutableStateFlow<com.example.signlanguagetranslator.ui.viewmodels.HomeState> _state = null;
+    @org.jetbrains.annotations.NotNull
+    private final kotlinx.coroutines.flow.StateFlow<com.example.signlanguagetranslator.ui.viewmodels.HomeState> state = null;
+    
+    @javax.inject.Inject
+    public HomeViewModel() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull
+    public final kotlinx.coroutines.flow.StateFlow<com.example.signlanguagetranslator.ui.viewmodels.HomeState> getState() {
+        return null;
+    }
+    
+    public final void onQueryChange(@org.jetbrains.annotations.NotNull
+    java.lang.String msg) {
+    }
+    
+    public final void sendPrompt() {
+    }
+}
